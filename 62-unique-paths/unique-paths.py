056ec29dc5e22aed2e9e1aed0452e4c376120a32
Wayne <690886723@qq.com>
2019-03-05 16:38:07 +0800

    first commit

---
+++ 62-unique-paths/unique-paths.py
@@ -0,0 +1,13 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        if m==0 or n==0:
+            return 0
+        res = [[0]*n for _ in range(m)]
+        for i in range(m):
+            res[i][0] = 1
+        for i in range(n):
+            res[0][i] = 1
+        for i in range(1,m):
+            for j in range(1,n):
+                res[i][j] = res[i-1][j] + res[i][j-1]
+        return res[-1][-1]
